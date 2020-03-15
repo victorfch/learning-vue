04fe16415bf911175c46659f7ae1bea0c78be3b7
victorfch <vfariachavez@gmail.com>
2020-03-15 22:53:01 +0000

    binding title

--- assets/js/inicio.js
+++ assets/js/inicio.js
@@ -1,6 +1,13 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hola Vue!'
-    }
-  })
+  el: '#app',
+  data: {
+    message: 'Hola Vue!'
+  }
+});
+
+var app2 = new Vue({
+  el: '#app2',
+  data: {
+    message: 'Cargada esta página el ' + new Date().toLocaleString()
+  }
+});
